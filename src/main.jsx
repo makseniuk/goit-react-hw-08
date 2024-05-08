d47b6b60dff6219754eb483b0825ee7a2d0ecff2
makseniuk <144346873+makseniuk@users.noreply.github.com>
2024-05-08 14:46:25 +0200

    1.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { HelmetProvider } from 'react-helmet-async';
-import 'modern-normalize';
-import './index.css';
-import App from './components/App';
-import { persistor, store } from './redux/store';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'modern-normalize'
+import App from './components/App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import { persistor, store } from './redux/store.js'
+import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </PersistGate>
-      </Provider>
+    <HelmetProvider><Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+    </Provider>
     </HelmetProvider>
-  </React.StrictMode>
-);
+    
+  </React.StrictMode>,
+)
