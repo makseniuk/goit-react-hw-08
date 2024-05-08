1cd811dfdc0e53ecabc46a01abd84d883cec3550
makseniuk <144346873+makseniuk@users.noreply.github.com>
2024-05-08 14:52:57 +0200

    1.3

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './components/App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { persistor, store } from './redux/store.js'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react'
 import { HelmetProvider } from 'react-helmet-async'
 
